a18f41a85b8e6ac85e27fbe2de00aabda0988b1e
Abhishek Boyane <abhishekboyaneco23859@gmail.com>
2026-02-02 00:29:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import random
+import os
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def mind_trick(request: Request):
-    # Random number to add for the trick
-    number = random.randint(1, 30)
-    answer = number / 2
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "number": number,
-        "answer": answer
-    })
+def home(request: Request):
+    number = random.randint(5, 50)
+    answer = number // 2
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "number": number,
+            "answer": answer
+        }
+    )
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
